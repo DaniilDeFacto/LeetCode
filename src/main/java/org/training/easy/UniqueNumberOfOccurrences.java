c36b5d4c4eb600842891a9582c623e1b7cad864d
Daniil Martynov <DaniilDeFacto@yandex.ru>
2024-08-10 15:30:24 +0400

    Add UniqueNumberOfOccurrences.java

---
+++ src/main/java/org/training/easy/UniqueNumberOfOccurrences.java
@@ -0,0 +1,15 @@
+package org.training.easy;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class UniqueNumberOfOccurrences {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer,Integer> numbersCount = new HashMap<>();
+        for (var number : arr) {
+            numbersCount.compute(number, (key, values) -> values == null ? 1 : values + 1);
+        }
+        return new HashSet<>(numbersCount.values()).size() == numbersCount.size();
+    }
+}
