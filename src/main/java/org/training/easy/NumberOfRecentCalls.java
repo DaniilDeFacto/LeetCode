13ca723a3c8487d2ca7aadad8490e3eeea25f984
Daniil Martynov <DaniilDeFacto@yandex.ru>
2024-08-13 16:04:12 +0400

    Add NumberOfRecentCalls.java

---
+++ src/main/java/org/training/easy/NumberOfRecentCalls.java
@@ -0,0 +1,22 @@
+package org.training.easy;
+
+import java.util.ArrayDeque;
+
+public class NumberOfRecentCalls {
+}
+
+class RecentCounter {
+    private final ArrayDeque<Integer> timesOfRequests;
+
+    public RecentCounter() {
+        this.timesOfRequests  = new ArrayDeque<>();
+    }
+
+    public int ping(int t) {
+        timesOfRequests.addLast(t);
+        while (timesOfRequests.getFirst() < t - 3000) {
+            timesOfRequests.removeFirst();
+        }
+        return timesOfRequests.size();
+    }
+}
