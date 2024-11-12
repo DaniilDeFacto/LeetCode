32546594e38d50aa1ec8b9895b6d3e4d6c9b49c4
Daniil Martynov <DaniilDeFacto@yandex.ru>
2024-11-12 16:42:32 +0400

    Add LengthOfLastWord.java

---
+++ src/main/java/org/training/Java/easy/LengthOfLastWord.java
@@ -0,0 +1,15 @@
+package org.training.Java.easy;
+
+public class LengthOfLastWord {
+    public int lengthOfLastWord(String s) {
+        int length = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                length++;
+            } else {
+                if (length > 0) return length;
+            }
+        }
+        return length;
+    }
+}
